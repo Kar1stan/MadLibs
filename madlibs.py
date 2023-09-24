5c3562d564348a7aa78c5296caf4b40c9456de05
Karastan Vladislav <90850632+Kar1stan@users.noreply.github.com>
2023-09-24 15:42:37 +0300

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,7 +1,11 @@
-adj = input("Adjective:")
-verb1 = input("Verb1:")
-verb2 = input("Verb2:")
-famous_person = input("Famous person:")
+try:
+    adj = input("Adjective:")
+    verb1 = input("Verb1:")
+    verb2 = input("Verb2:")
+    famous_person = input("Famous person:")
+
+except EOFError as e:
+    print(e)
 
 madlib = f"Computer programming is so {adj}! I love to {verb1}.Stay hydrated and {verb2} like you are {famous_person}!"
 
